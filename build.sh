7451dc0d9a54f6ab0b26f007667d142dec3118d6
YoungCoding <34829927+YoungCoding@users.noreply.github.com>
2024-12-08 07:51:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -35,10 +35,10 @@ PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
 PACKAGES="$PACKAGES luci-app-fileassistant"
 # 硬盘休眠
 PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
-# p910nd 打印服务器
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
+# usb 打印服务器
+PACKAGES="$PACKAGES luci-i18n-usb-printer-zh-cn"
 # GoWebDav
-PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
 # Samba 网络共享
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # qBittorrent 下载器
